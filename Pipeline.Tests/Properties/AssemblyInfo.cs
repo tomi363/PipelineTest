0244e78aeeb0400f7558757f210006383168a460
zakan <tomi@smartlabs.be>
2025-06-26 20:58:17 +0300

    Added test project

---
+++ Pipeline.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7460718-f5cb-475d-b049-661253bc9852")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pipeline.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pipeline.Tests
+{
+    [TestClass]
+    public class ExampleTests
+    {
+        [TestMethod]
+        public void SampleTest_ShouldPass()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
